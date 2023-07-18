cd2628abe7a3f34d3e944d4f2c5c1e34cc543274
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-18 11:09:21 -0700

    Initial Commit

--- CS131-SwitchExercise/CS131-SwitchExercise.cpp
+++ CS131-SwitchExercise/CS131-SwitchExercise.cpp
@@ -1,20 +1,15 @@
-// CS131-SwitchExercise.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+/*
+Create 4000 random numbers between 1-4, and build a count for the
+total number of each of the 4 numbers were chosen. We would hope
+that the frequency would be around 1000 times for each of the
+numbers in our selected range.
+*/
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
