b38240de650e634502e60800e1f6afc495160370
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-18 11:29:39 -0700

    add breaks in switch cases

--- CS131-SwitchExercise/CS131-SwitchExercise.cpp
+++ CS131-SwitchExercise/CS131-SwitchExercise.cpp
@@ -11,6 +11,7 @@ numbers in our selected range.
 
 int main()
 {
+    srand(time(0));
     int ones = 0;
     int twos = 0;
     int threes = 0;
@@ -18,23 +19,31 @@ int main()
     int numCount = 0;
     int randomNum = 0;
 
-    while (numCount <= 4000)
+    while (numCount < 4000)
     {
         randomNum = (rand() % 4) + 1;
         switch (randomNum)
         {
         case 1:
             ones++;
+            break;
         case 2:
             twos++;
+            break;
         case 3:
             threes++;
+            break;
         case 4:
             fours++;
+            break;
         }
         numCount++;
     }
     
-    // final output
+    cout << "Here is the results of 4000 random numbers with values 1 - 4:\n";
+    cout << "ones: "   << ones   << endl;
+    cout << "twos: "   << twos   << endl;
+    cout << "threes: " << threes << endl;
+    cout << "fours: "  << ones   << endl;
 
 }
